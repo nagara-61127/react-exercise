9cb19905f3227d6748320115af439d9de033cc0b
boshooooooooton <un.cuisinier.france@gmail.com>
2023-02-23 22:59:33 +0900

    prettierによるコードの整形

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from "react";
-import "./index.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from 'react';
+import './index.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-	faChevronRight,
-	faChevronLeft,
-	faCircle,
-	faCheckCircle,
-	faPlus,
-} from "@fortawesome/free-solid-svg-icons";
+  faChevronRight,
+  faChevronLeft,
+  faCircle,
+  faCheckCircle,
+  faPlus,
+} from '@fortawesome/free-solid-svg-icons';
 
 type Item = {
   itemName: string;
   quantity: number;
   isSelected: boolean;
-}
+};
 
 const App = () => {
-	const [items, setItems] = useState<Item[]>([]);
-  const [inputValue, setInputValue] = useState("");
+  const [items, setItems] = useState<Item[]>([]);
+  const [inputValue, setInputValue] = useState('');
   const [totalItemCount, setTotalItemCount] = useState(0);
 
   const handleAddButtonClick = () => {
@@ -29,69 +29,85 @@ const App = () => {
 
     const newItems = [...items, newItem];
     setItems(newItems);
-    setInputValue("");
+    setInputValue('');
     culculateTotal();
-  }
+  };
 
   const toggleChange = (index: number) => {
     const changeItems = [...items];
     changeItems[index].isSelected = !changeItems[index].isSelected;
     setItems(changeItems);
     culculateTotal();
-  }
+  };
 
-  const handleChangeQuantity = (index: number, fluctuation: 'increase' | 'decrease') => {
+  const handleChangeQuantity = (
+    index: number,
+    fluctuation: 'increase' | 'decrease'
+  ) => {
     const changeItems = [...items];
-    if(fluctuation==='decrease' && changeItems[index].quantity===0){
-      if(window.confirm('アイテムを削除しますか？')){
+    if (fluctuation === 'decrease' && changeItems[index].quantity === 0) {
+      if (window.confirm('アイテムを削除しますか？')) {
         deleteItem(index);
       }
     } else {
-      switch(fluctuation) {
-      case 'increase':
-        changeItems[index].quantity++;
-        break;
-      case 'decrease':
-        changeItems[index].quantity--;
-        break;
+      switch (fluctuation) {
+        case 'increase':
+          changeItems[index].quantity++;
+          break;
+        case 'decrease':
+          changeItems[index].quantity--;
+          break;
       }
       setItems(changeItems);
     }
     culculateTotal();
-  }
+  };
 
   const deleteItem = (index: number) => {
-    const test = items;
     const changeItems = [...items];
     changeItems.splice(index, 1);
     setItems(changeItems);
-  }
+  };
 
   const culculateTotal = () => {
     let total = 0;
-    items.map(item => total = total + item.quantity);
+    items.map((item) => (total = total + item.quantity));
     setTotalItemCount(total);
-  }
+  };
 
-	return (
-		<div className="app-background">
-			<div className="main-container">
-				<div className="add-item-box">
-					<input value={inputValue} onChange={(event) => setInputValue(event.target.value)} className="add-item-input" placeholder="Add an item..." />
-					<FontAwesomeIcon icon={faPlus} onClick={() => handleAddButtonClick()} />
-				</div>
-				<div className="item-list">
+  return (
+    <div className="app-background">
+      <div className="main-container">
+        <div className="add-item-box">
+          <input
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+            className="add-item-input"
+            placeholder="Add an item..."
+          />
+          <FontAwesomeIcon
+            icon={faPlus}
+            onClick={() => handleAddButtonClick()}
+          />
+        </div>
+        <div className="item-list">
           {items.map((item, index) => (
             <div className="item-container">
               <div className="item-name">
                 {item.isSelected ? (
                   <>
-                    <FontAwesomeIcon icon={faCheckCircle} onClick={() => toggleChange(index)} />
+                    <FontAwesomeIcon
+                      icon={faCheckCircle}
+                      onClick={() => toggleChange(index)}
+                    />
                     <span className="completed">{item.itemName}</span>
                   </>
-                ):(
+                ) : (
                   <>
-                    <FontAwesomeIcon icon={faCircle} onClick={() => toggleChange(index)} />
+                    <FontAwesomeIcon
+                      icon={faCircle}
+                      onClick={() => toggleChange(index)}
+                    />
                     <span>{item.itemName}</span>
                   </>
                 )}
@@ -107,12 +123,12 @@ const App = () => {
               </div>
             </div>
           ))}
-					<div className="item-container"></div>
-				</div>
-				<div className="total">Total: {totalItemCount}</div>
-			</div>
-		</div>
-	);
+          <div className="item-container"></div>
+        </div>
+        <div className="total">Total: {totalItemCount}</div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
